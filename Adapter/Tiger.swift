601390361365f9d965f8077ef208cbea221fb26f
wansook0316 <wansook0316@gmail.com>
2022-08-23 09:53:48 +0900

    Add: Tiger 레거시 코드 추가

--- Adapter/Tiger.swift
+++ Adapter/Tiger.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+// Legacy코드인 상황을 가정 (일부러 코드를 이상하게 짬)
+internal class Tiger {
+
+    internal func updateNickname(to nickname: String) {
+        self.nickname = nickname
+    }
+
+    internal func getNickname() -> String {
+        return self.nickname
+    }
+
+    internal func roar() {
+        print("크아앙 어흥")
+    }
+
+    private var nickname: String = "이름 없음"
+    
+}
